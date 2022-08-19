8ad35c806ed6afdc6918b0e1b93621274192b25c
tomozo6 <liebestraum.tomo@gmail.com>
2022-08-19 15:56:18 +0900

    :sparkles: 支払総額取得APIを新規作成

--- backend/libra-api/tests/test_app.py
+++ backend/libra-api/tests/test_app.py
@@ -26,3 +26,31 @@ def test_pay_recoreds_post(fixture_client, fixture_dynamodb):
 
     assert response.status_code == 200
     assert response.json_body == {'Payer': 'tomozo', 'Amount': 1000, 'Memo': '6/6 ハナマサで購入', 'InputDate': '2022-06-06 09:00:00+09:00'}
+
+
+def test_pay_amount_by_payer(fixture_client, fixture_dynamodb):
+    # テストデータ投入
+    fixture_client.http.post(
+        '/pay/records',
+        headers={'Content-Type': 'application/json'},
+        body=json.dumps({'Payer': 'tomozo', 'Amount': 1000, 'Memo': '6/6 ハナマサで購入'})
+    )
+    fixture_client.http.post(
+        '/pay/records',
+        headers={'Content-Type': 'application/json'},
+        body=json.dumps({'Payer': 'tomozo', 'Amount': 111, 'Memo': '6/6 ハナマサで購入'})
+    )
+    fixture_client.http.post(
+        '/pay/records',
+        headers={'Content-Type': 'application/json'},
+        body=json.dumps({'Payer': 'sayaka', 'Amount': 222222, 'Memo': '6/6 ハナマサで購入'})
+    )
+
+    # Get Amount
+    response = fixture_client.http.get(
+        '/pay/amount/tomozo',
+        headers={'Content-Type': 'application/json'}
+    )
+
+    assert response.status_code == 200
+    assert response.json_body == {'Payer': 'tomozo', 'Amount': 1111}
